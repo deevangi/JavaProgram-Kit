25843c57a4d4847df3b6bf6759ee7a04b0e0c357
deevangi <deevangibaghela112@gmail.com>
2024-07-05 09:50:31 +0530

    add new files

---
+++ src/LB_006.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class LB_006 {
+
+    public static void main(String[] args){
+        int[] marks = new int[4];
+                marks[0]=65;
+                marks[1]=75;
+                marks[2]=89;
+                marks[3]=77;
+
+                System.out.println(marks[0]);
+                System.out.println(marks[1]);
+                System.out.println(marks[2]);
+                System.out.println(marks[3]);
+
+
+
+    }
+}
